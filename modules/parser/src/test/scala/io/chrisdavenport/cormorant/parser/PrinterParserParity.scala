18cbaf70081416160f2f43dae7ab9afd566cdecd
Chris Davenport <chris@christopherdavenport.tech>
2018-07-13 12:26:28 -0400

    Full Round-tripping property tests

--- modules/parser/src/test/scala/io/chrisdavenport/cormorant/parser/PrinterParserParity.scala
+++ modules/parser/src/test/scala/io/chrisdavenport/cormorant/parser/PrinterParserParity.scala
@@ -8,7 +8,7 @@ import org.scalacheck._
 class PrinterParserParity extends mutable.Specification with ScalaCheck {
 
   implicit val arbField : Arbitrary[CSV.Field] = Arbitrary(
-    Gen.listOf(Gen.asciiChar).map(_.mkString).map(_.replace("\"", "")).map(CSV.Field.apply)
+    Gen.listOf(Gen.asciiChar).map(_.mkString).map(CSV.Field.apply)
   )
   implicit val arbRow: Arbitrary[CSV.Row] = Arbitrary(
     for {
@@ -17,17 +17,66 @@ class PrinterParserParity extends mutable.Specification with ScalaCheck {
     } yield CSV.Row(field :: list)
   )
 
+  implicit val arbRows :Arbitrary[CSV.Rows] = Arbitrary(
+    for {
+      row <- Arbitrary.arbitrary[CSV.Row]
+      l <- Gen.listOf(Arbitrary.arbitrary[CSV.Row])
+    } yield CSV.Rows(row :: l)
+  )
+
+  implicit val arbHeader : Arbitrary[CSV.Header] = Arbitrary(
+    Gen.listOf(Gen.asciiChar).map(_.mkString).map(CSV.Header.apply)
+  )
+
+  implicit val arbHeaders : Arbitrary[CSV.Headers] = Arbitrary(
+    for {
+      header <- Arbitrary.arbitrary[CSV.Header]
+      list <- Gen.listOf(Arbitrary.arbitrary[CSV.Header])
+    } yield CSV.Headers(header :: list)
+  )
+
+  implicit val arbComplete : Arbitrary[CSV.Complete] = Arbitrary(
+    for {
+      headers <- Arbitrary.arbitrary[CSV.Headers]
+      rows <- Arbitrary.arbitrary[CSV.Rows]
+    } yield CSV.Complete(headers, rows)
+  )
+
   "Printer should round trip with parser" in {
-    "field should round trip" in  prop {  field : CSV.Field =>
+    "field should round trip" in  prop {  a : CSV.Field =>
       import _root_.io.chrisdavenport.cormorant.implicits._
-      val encoded = field.print(Printer.default)
-      parseField(encoded) must_=== Either.right(field)
+      val encoded = a.print(Printer.default)
+      parseField(encoded) must_=== Either.right(a)
     }
 
-    "row should round trip" in prop { row: CSV.Row => 
+    "row should round trip" in prop { a: CSV.Row => 
       import _root_.io.chrisdavenport.cormorant.implicits._
-      val encoded = row.print(Printer.default)
-      parseRow(encoded) must_=== Either.right(row)
+      val encoded = a.print(Printer.default)
+      parseRow(encoded) must_=== Either.right(a)
+    }
+
+    "rows should round trip" in prop { a: CSV.Rows => 
+      import _root_.io.chrisdavenport.cormorant.implicits._
+      val encoded = a.print(Printer.default)
+      parseRows(encoded) must_=== Either.right(a)
+    }
+
+    "header should round trip" in prop {a: CSV.Header => 
+      import _root_.io.chrisdavenport.cormorant.implicits._
+      val encoded = a.print(Printer.default)
+      parseHeader(encoded) must_=== Either.right(a)
+    }
+
+    "headers should round trip" in prop {a: CSV.Headers => 
+      import _root_.io.chrisdavenport.cormorant.implicits._
+      val encoded = a.print(Printer.default)
+      parseHeaders(encoded) must_=== Either.right(a)
+    }
+
+    "complete should round trip" in prop {a: CSV.Complete => 
+      import _root_.io.chrisdavenport.cormorant.implicits._
+      val encoded = a.print(Printer.default)
+      parseComplete(encoded) must_=== Either.right(a)
     }
 
   }
