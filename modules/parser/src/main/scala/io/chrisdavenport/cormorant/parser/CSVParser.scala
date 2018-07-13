a1479c3647de7af40cec23d07d932cdb47b13558
Christopher Davenport <Chris@ChristopherDavenport.tech>
2018-07-12 21:40:04 -0400

    Add CSV Parser Specifications

--- modules/parser/src/main/scala/io/chrisdavenport/cormorant/parser/CSVParser.scala
+++ modules/parser/src/main/scala/io/chrisdavenport/cormorant/parser/CSVParser.scala
@@ -6,6 +6,66 @@ import atto._
 import Atto._
 import cats.implicits._
 
+/**
+  * This CSVParser tries to stay fairly close to the initial specification
+  * https://tools.ietf.org/html/rfc4180
+  *
+  * Deviations from the specification, here I have chosen to use a 
+  * permissive CRLF that will accept a CRLF or a LF.
+  *
+  * The important details are as follows
+  * 1.  Each record is located on a separate line, delimited by a line
+  *     break (CRLF).  For example:
+  *
+  *     aaa,bbb,ccc CRLF
+  *     zzz,yyy,xxx CRLF
+  *
+  * 2.  The last record in the file may or may not have an ending line
+  *     break.  For example:
+  *
+  *     aaa,bbb,ccc CRLF
+  *     zzz,yyy,xxx
+  *
+  * 3.  There maybe an optional header line appearing as the first line
+  *     of the file with the same format as normal record lines.  This
+  *     header will contain names corresponding to the fields in the file
+  *     and should contain the same number of fields as the records in
+  *     the rest of the file (the presence or absence of the header line
+  *     should be indicated via the optional "header" parameter of this
+  *     MIME type).  For example:
+  *
+  *     field_name,field_name,field_name CRLF
+  *     aaa,bbb,ccc CRLF
+  *     zzz,yyy,xxx CRLF
+  * 4.  Within the header and each record, there may be one or more
+  *     fields, separated by commas.  Each line should contain the same
+  *     number of fields throughout the file.  Spaces are considered part
+  *     of a field and should not be ignored.  The last field in the
+  *     record must not be followed by a comma.  For example:
+  *
+  *     aaa,bbb,ccc
+  *
+  * 5.  Each field may or may not be enclosed in double quotes (however
+  *     some programs, such as Microsoft Excel, do not use double quotes
+  *     at all).  If fields are not enclosed with double quotes, then
+  *     double quotes may not appear inside the fields.  For example:
+  *
+  *     "aaa","bbb","ccc" CRLF
+  *     zzz,yyy,xxx
+  *
+  * 6.  Fields containing line breaks (CRLF), double quotes, and commas
+  *     should be enclosed in double-quotes.  For example:
+  *
+  *     "aaa","b CRLF
+  *     bb","ccc" CRLF
+  *     zzz,yyy,xxx
+  *
+  * 7.  If double-quotes are used to enclose fields, then a double-quote
+  *     appearing inside a field must be escaped by preceding it with
+  *     another double quote.  For example:
+  *
+  *     "aaa","b""bb","ccc"
+  **/
 object CSVParser {
 
   val dquote : Char = '\"'
