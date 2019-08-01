aa9a13bf74dacec83988336584e690e10d0c1ae2
Lorand Szakacs <lsz@lorandszakacs.com>
2019-08-01 12:08:44 +0300

    Fix compile error by fully qualifying name

--- modules/http4s/src/main/scala/io/chrisdavenport/cormorant/http4s/package.scala
+++ modules/http4s/src/main/scala/io/chrisdavenport/cormorant/http4s/package.scala
@@ -10,93 +10,117 @@ import org.http4s._
 
 package object http4s {
 
-  implicit def completeEntityEncoder[F[_]: Applicative]( implicit
-    printer: Printer = Printer.default,
-    mediaType: MediaType = MediaType.text.csv
+  implicit def completeEntityEncoder[F[_]: Applicative](
+      implicit
+      printer: Printer = Printer.default,
+      mediaType: MediaType = MediaType.text.csv
   ): EntityEncoder[F, CSV.Complete] = {
     val contentTypeHeader: Header = headers.`Content-Type`(mediaType)
-    EntityEncoder.encodeBy(Headers.of(contentTypeHeader))(csvComplete =>
-      Entity(Stream(printer.print(csvComplete)).through(text.utf8Encode).covary[F])
+    EntityEncoder.encodeBy(Headers.of(contentTypeHeader))(
+      csvComplete => Entity(Stream(printer.print(csvComplete)).through(text.utf8Encode).covary[F])
     )
   }
-  implicit def rowsEntityEncoder[F[_]: Applicative]( implicit
-    printer: Printer = Printer.default,
-    mediaType: MediaType = MediaType.text.csv
+  implicit def rowsEntityEncoder[F[_]: Applicative](
+      implicit
+      printer: Printer = Printer.default,
+      mediaType: MediaType = MediaType.text.csv
   ): EntityEncoder[F, CSV.Rows] = {
     val contentTypeHeader: Header = headers.`Content-Type`(mediaType)
-    EntityEncoder.encodeBy(Headers.of(contentTypeHeader))(csvRows =>
-      Entity(Stream(printer.print(csvRows)).through(text.utf8Encode).covary[F])
+    EntityEncoder.encodeBy(Headers.of(contentTypeHeader))(
+      csvRows => Entity(Stream(printer.print(csvRows)).through(text.utf8Encode).covary[F])
     )
   }
 
-  implicit def streamEncodeRows[F[_]: Applicative]( implicit
-    p: Printer = Printer.default,
-    mediaType: MediaType = MediaType.text.csv
+  implicit def streamEncodeRows[F[_]: Applicative](
+      implicit
+      p: Printer = Printer.default,
+      mediaType: MediaType = MediaType.text.csv
   ): EntityEncoder[F, Stream[F, CSV.Row]] = {
     val contentTypeHeader: Header = headers.`Content-Type`(mediaType)
-    EntityEncoder.encodeBy(Headers.of(contentTypeHeader))(s =>
-      Entity(
-        s.through(encodeRows(p))
-        .intersperse("\r\n")
-        .through(text.utf8Encode)
-      )
+    EntityEncoder.encodeBy(Headers.of(contentTypeHeader))(
+      s =>
+        Entity(
+          s.through(encodeRows(p))
+            .intersperse("\r\n")
+            .through(text.utf8Encode)
+        )
     )
   }
 
   def streamEncodeWrite[F[_]: Applicative, A: Write](
-    implicit
-    p: Printer = Printer.default,
-    mediaType: MediaType = MediaType.text.csv
+      implicit
+      p: Printer = Printer.default,
+      mediaType: MediaType = MediaType.text.csv
   ): EntityEncoder[F, Stream[F, A]] =
     streamEncodeRows[F].contramap(_.map(Write[A].write))
 
   def streamEncodeLabelledWrite[F[_]: Applicative, A: LabelledWrite](
-    p: Printer = Printer.default,
-    mediaType: MediaType = MediaType.text.csv
+      p: Printer = Printer.default,
+      mediaType: MediaType = MediaType.text.csv
   ): EntityEncoder[F, Stream[F, A]] = {
     val contentTypeHeader: Header = headers.`Content-Type`(mediaType)
-    EntityEncoder.encodeBy(Headers.of(contentTypeHeader))(s =>
-      Entity(
-        s.through(writeLabelled(p))
-        .intersperse("\r\n")
-        .through(text.utf8Encode)
-      )
+    EntityEncoder.encodeBy(Headers.of(contentTypeHeader))(
+      s =>
+        Entity(
+          s.through(writeLabelled(p))
+            .intersperse("\r\n")
+            .through(text.utf8Encode)
+        )
     )
   }
 
   implicit def completeEntityDecoder[F[_]: Sync]: EntityDecoder[F, CSV.Complete] =
-    new EntityDecoder[F, CSV.Complete]{
+    new EntityDecoder[F, CSV.Complete] {
       def consumes: Set[MediaRange] = Set(MediaType.text.csv)
-      def decode(msg: Message[F],strict: Boolean): DecodeResult[F,CSV.Complete] = cats.data.EitherT{
-        msg.body.through(text.utf8Decode).compile.foldMonoid.map(s =>
-          parser.parseComplete(s).leftMap(parseError => org.http4s.MalformedMessageBodyFailure(parseError.reason))
-        )
-      }
+      def decode(msg: Message[F], strict: Boolean): DecodeResult[F, CSV.Complete] =
+        cats.data.EitherT {
+          msg.body
+            .through(text.utf8Decode)
+            .compile
+            .foldMonoid
+            .map(
+              s =>
+                _root_.io.chrisdavenport.cormorant.parser
+                  .parseComplete(s)
+                  .leftMap(parseError => org.http4s.MalformedMessageBodyFailure(parseError.reason))
+            )
+        }
     }
 
   implicit def rowsEntityDecoder[F[_]: Sync]: EntityDecoder[F, CSV.Rows] =
-    new EntityDecoder[F, CSV.Rows]{
+    new EntityDecoder[F, CSV.Rows] {
       def consumes: Set[MediaRange] = Set(MediaType.text.csv)
-      def decode(msg: Message[F],strict: Boolean): DecodeResult[F,CSV.Rows] = cats.data.EitherT{
-        msg.body.through(text.utf8Decode).compile.foldMonoid.map(s =>
-          parser.parseRows(s).leftMap(parseError => org.http4s.MalformedMessageBodyFailure(parseError.reason))
-        )
+      def decode(msg: Message[F], strict: Boolean): DecodeResult[F, CSV.Rows] = cats.data.EitherT {
+        msg.body
+          .through(text.utf8Decode)
+          .compile
+          .foldMonoid
+          .map(
+            s =>
+              _root_.io.chrisdavenport.cormorant.parser
+                .parseRows(s)
+                .leftMap(parseError => org.http4s.MalformedMessageBodyFailure(parseError.reason))
+          )
       }
     }
 
   def streamingLabelledReadDecoder[F[_]: Sync, A: LabelledRead]: EntityDecoder[F, Stream[F, A]] =
-    new EntityDecoder[F, Stream[F, A]]{
+    new EntityDecoder[F, Stream[F, A]] {
       def consumes: Set[MediaRange] = Set(MediaType.text.csv)
-      def decode(msg: Message[F],strict: Boolean): DecodeResult[F,Stream[F, A]] =
-        msg.body.through(text.utf8Decode).through(readLabelled[F, A])
+      def decode(msg: Message[F], strict: Boolean): DecodeResult[F, Stream[F, A]] =
+        msg.body
+          .through(text.utf8Decode)
+          .through(readLabelled[F, A])
           .pure[DecodeResult[F, ?]]
     }
 
   def streamingReadDecoder[F[_]: Sync, A: Read]: EntityDecoder[F, Stream[F, A]] =
-    new EntityDecoder[F, Stream[F, A]]{
+    new EntityDecoder[F, Stream[F, A]] {
       def consumes: Set[MediaRange] = Set(MediaType.text.csv)
-      def decode(msg: Message[F],strict: Boolean): DecodeResult[F,Stream[F, A]] =
-        msg.body.through(text.utf8Decode).through(readRows[F, A])
+      def decode(msg: Message[F], strict: Boolean): DecodeResult[F, Stream[F, A]] =
+        msg.body
+          .through(text.utf8Decode)
+          .through(readRows[F, A])
           .pure[DecodeResult[F, ?]]
     }
 }
--- modules/http4s/src/test/scala/io/chrisdavenport/cormorant/http4s/Http4sSpec.scala
+++ modules/http4s/src/test/scala/io/chrisdavenport/cormorant/http4s/Http4sSpec.scala
@@ -9,20 +9,20 @@ import org.http4s.implicits._
 
 class Http4sSpec extends CormorantSpec {
   "Http4s Entity Encoder/Decoder" should {
-    "round trip rows" in prop { rows : CSV.Rows => 
+    "round trip rows" in prop { rows: CSV.Rows =>
       val service = HttpRoutes.of[IO] {
         case _ => Ok(rows)
       }
       val client = Client.fromHttpApp(service.orNotFound)
       client.expect[CSV.Rows]("").unsafeRunSync must_=== rows
     }.set(minTestsOk = 20, workers = 2)
-    "round trip complete" in prop { rows : CSV.Complete => 
-      val service = HttpRoutes.of[IO]{
+    "round trip complete" in prop { rows: CSV.Complete =>
+      val service = HttpRoutes.of[IO] {
         case _ => Ok(rows)
       }
       val client = Client.fromHttpApp(service.orNotFound)
       client.expect[CSV.Complete]("").unsafeRunSync must_=== rows
     }.set(minTestsOk = 20, workers = 2)
   }
-  
+
 }
