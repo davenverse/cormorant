27669f32550f17d98947509874b07af2aada7495
Alejandro Hernández <info@alejandrohdezma.com>
2019-08-07 18:21:22 +0200

    Add docs for the method

--- modules/core/src/main/scala/io/chrisdavenport/cormorant/syntax/put.scala
+++ modules/core/src/main/scala/io/chrisdavenport/cormorant/syntax/put.scala
@@ -6,6 +6,18 @@ trait put {
 
   implicit class putOps[A](a: A) {
 
+    /**
+      * Facilitates the transformation of any `A` with a `Put`
+      * instance into a field
+      *
+      * @example {{{
+      * //Before
+      * Put[String].put("hello")
+      *
+      * //After
+      * "Hello".field
+      * }}}
+      */
     def field(implicit P: Put[A]): CSV.Field = P.put(a)
 
   }
