49360676d202d52d13f54e09e2f5059e817df2d9
Alejandro Hernández <info@alejandrohdezma.com>
2019-08-07 18:21:14 +0200

    Move implicit to `field` method
    So when an implicit is missing the error will be `couldn't find implicit` instead of `missing "field" value`

--- modules/core/src/main/scala/io/chrisdavenport/cormorant/syntax/put.scala
+++ modules/core/src/main/scala/io/chrisdavenport/cormorant/syntax/put.scala
@@ -3,9 +3,13 @@ package io.chrisdavenport.cormorant.syntax
 import io.chrisdavenport.cormorant.{CSV, Put}
 
 trait put {
-  implicit class putOps[A: Put](a: A) {
-    def field: CSV.Field = Put[A].put(a)
+
+  implicit class putOps[A](a: A) {
+
+    def field(implicit P: Put[A]): CSV.Field = P.put(a)
+
   }
+
 }
 
 object put extends put
